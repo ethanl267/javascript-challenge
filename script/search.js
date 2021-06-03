fa6cb161e7e9932c563697229b0312ff29b257e1
ethan <ethanlesch8@gmail.com>
2021-06-03 16:18:21 +0200

    push

--- script/search.js
+++ script/search.js
@@ -1,10 +1,52 @@
+
 // fetching the API
-const url = ('https://newsapi.org/v2/everything?q=tesla&apiKey=01a4ed9cbe1b4f048457868df3864029')
-fetch(url)
 
-.then((Response) => Response.json())
-.then(json => console.log(json))
+// const url = ('https://newsapi.org/v2/everything?q=tesla&apiKey=01a4ed9cbe1b4f048457868df3864029')
 
-function myfunction() {
-    var 
+// fetch api data
+function myFunction(){
+    // get user input
+    let stuff;
+    const searchThing = document.getElementsByTagName("input")[0].value;
+    // const proxy = "http://cors-anywhere.herokuapp.com/"
+    const url = `https://newsapi.org/v2/everything?q=${searchThing}&apiKey=01a4ed9cbe1b4f048457868df3864029`
+    console.log(searchThing)
+    // const all = proxy + url
+    const request = new Request(url)
+    
+    // console.log(searchThing)
+    fetch(request)
+    
+    .then((Response) => Response.json())
+    .then(json => {
+        console.log("------>",json)
+        stuff=json.articles;
+        
+        for (i in stuff){
+            console.log(stuff[i]);
+            
+            document.getElementById("show").innerHTML += `
+            <div>
+                <img src=${stuff[i].urlToImage}/>
+                <p>${stuff[i].description}</p>
+            </div>
+            `
+        }
+    })
+//     .catch(error => {
+//         console.log(error);
+// });
+
 }
+
+//  display data through browser
+showSearch = search => {
+    const searchDiv = document.getElementById('#search');
+    articles.forEach(search=> {
+        const stuff = document.getElementById("show").innerHTML;
+        stuff+=search;
+
+        
+    })
+}
+
---
+++ script/temp.js
@@ -0,0 +1,107 @@
+let products = [];
+function getPosts() {
+  // Get element to change
+  let list = document.getElementById("li-items");
+
+  // Fetch the data
+  fetch(`https://newsapi.org/v2/everything?q=${searchThing}&apiKey=01a4ed9cbe1b4f048457868df3864029`)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      json.article.forEach((item) => {
+        products.push(item);
+        createsaleItem(item);
+      });
+    });
+}
+
+function createsaleItem(article) {
+  const listitem = `<li>
+  <div class="product-card">
+  <img src=${article.urlToImage}/>
+  <h1 class="itemName">${article.title}</h1>
+  <p class="price">${article.desciption}</p>
+</div></li>`;
+  let list = document.getElementById("li-items");
+  list.innerHTML += listitem;
+}
+
+getPosts();
+
+function searchProducts() {
+    // Get data from form
+    let form = document.getElementById("search-form");
+    let searchTerm = form.getElementsByTagName("input")[0].value;
+    // Create Regular Expression
+    let searchRegEx = new RegExp(searchTerm, "i");
+    // Fetch data to search from
+    fetch("https://shielded-woodland-34724.herokuapp.com/show-records/")
+      .then((response) => response.json())
+      .then((json) => {
+        // Filter data
+        let result = json.filter((product) => {
+          return article
+        });
+        // Check if there are found results
+        if (result.length > 0) {
+          // Render items that matched
+          document.getElementById("li-items").innerHTML = "";
+          result.forEach((product) => createsaleItem(product));
+        } else {
+          // Alert nothing found
+          alert("nothing found");
+        }
+      })
+      .catch((err) => {
+        // Log any error from back end
+        console.log(err);
+      });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------
+
+function searchProducts() {
+    // Get data from form
+    let form = document.getElementById("search-form");
+    let searchTerm = form.getElementsByTagName("input")[0].value;
+    // Create Regular Expression
+    let searchRegEx = new RegExp(searchTerm, "i");
+    // Fetch data to search from
+    fetch("https://shielded-woodland-34724.herokuapp.com/show-records/")
+      .then((response) => response.json())
+      .then((json) => {
+        // Filter data
+        let result = json.article.filter((product) => {
+          return product.product_name.search(searchRegEx) !== -1;
+        });
+        // Check if there are found results
+        if (result.length > 0) {
+          // Render items that matched
+          document.getElementById("li-items").innerHTML = "";
+          result.forEach((product) => createsaleItem(product));
+        } else {
+          // Alert nothing found
+          alert("nothing found");
+        }
+      })
+      .catch((err) => {
+        // Log any error from back end
+        console.log(err);
+      });
+  }
